4feac142dd7cfad04b51ff84c040cc44ff0e5478
chetex <chetex69@gmail.com>
2018-09-15 17:31:45 +0200

    First component in React from scratch

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,15 +1,13 @@
+// Extract react from "node modules" and add to React var
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component
+// and this component produce some HMTL
+const App = function () {
+    return <div>Hola</div>; 
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component generated HTML and put it 
+// on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
