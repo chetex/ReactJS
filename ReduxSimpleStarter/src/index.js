af68d007ac36947c012af9cb46d22a70aa1ac4c2
chetex <chetex69@gmail.com>
2018-09-15 18:01:45 +0200

    addin youtube api key

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -2,9 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// This key allow us call youtube api
+const API_KEY = 'AIzaSyC3Q5YmxCqGB7BGC8ovydEN-T69Dknob5s';
+
 // Create a new component
 // and this component produce some HMTL
-const App = function () {
+const App = () => {
     return <div>Hola</div>; 
 }
 
